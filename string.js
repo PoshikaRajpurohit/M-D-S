0b3ca77dce85cccde5289fb5f5ebf3050a70f297
PoshikaRajpurohit <159758700+PoshikaRajpurohit@users.noreply.github.com>
2024-12-30 10:44:09 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,46 @@
+// STRING 
+// let m="map";
+// console.log(m);
+// let p='pam';
+// console.log(p);
+// let mp=`Best-Friends`
+//         console.log(mp);
+//         console.log(mp.length);
+
+// 1 slice-method
+    // console.log(mp.slice(0,2));
+    
+// 2 substr method
+    // console.log(mp.substr(0,6));
+    
+// 3 UPPERCASE-METHOD
+    // console.log(mp.toUpperCase());
+
+
+// 4 LOWERCASE-METHOD
+    // console.log(mp.toLowerCase());
+
+
+
+    
+// DATE-OBJECT
+// let date= new Date();
+// console.log(date);
+
+
+
+
+// MATH-FUNCTION
+    console.log(Math.floor(2.6));
+    console.log(Math.ceil(2.2));
+    console.log(Math.round(9.2));
+    console.log(Math.round(9.8));
+    console.log(Math.sqrt(121));
+    console.log(Math.pow(2,5));
+    console.log(Math.random()*20);
+    
+
+
+
+        
+
